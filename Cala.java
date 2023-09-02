a7984ac16fc339dadbe2e769b77a8e3f2720097f
sk99112 <129582261+sk99112k@users.noreply.github.com>
2023-09-02 15:52:30 +0900

    パッケージ名クラス名import

--- Cala.java
+++ Cala.java
@@ -1,10 +1,11 @@
 package calcapp.main;
+import calcapp.logics.CalcLogic; //CalaLogicという表記があったら、calcapp.logics.CalcLogicのことだと解釈しなさいという意味
 public class Cala {
   public static void main(String[] args) {
     int a = 10;
     int b = 2;
-    int total = calcapp.logics.CalcLogic.tasu(a, b);
-    int delta = calcapp.logics.CalcLogic.hiku(a, b);
+    int total = CalcLogic.tasu(a, b); //FQCNでなくてもエラーにならない
+    int delta = calcapp.logics.CalcLogic.hiku(a, b); //FQCNを指定しても良い
     System.out.println("足すと" + total + "、引くと" + delta);
   }
 }
