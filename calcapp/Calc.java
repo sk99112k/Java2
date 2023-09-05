0d459dde3a0dec3ec457562384c521cc4a2675b1
sk99112 <129582261+sk99112k@users.noreply.github.com>
2023-09-05 14:30:36 +0900

    パッケージ

--- calcapp/Calc.java
+++ calcapp/Calc.java
@@ -1,12 +1,14 @@
-package calcapp.main;
-import calcapp.logics.CalcLogic; //CalaLogicという表記があったら、calcapp.logics.CalcLogicのことだと解釈しなさいという意味
-//import calcapp.logics.*;  //calcapp.logicsパッケージに属するすべてのクラスをインポートしたい場合
+package calcapp;
+
+
+import calcapp.logics.CalcLogic;
+
 public class Calc {
   public static void main(String[] args) {
     int a = 10;
     int b = 2;
     int total = CalcLogic.tasu(a, b); //FQCNでなくてもエラーにならない
-    int delta = calcapp.logics.CalcLogic.hiku(a, b); //FQCNを指定しても良い
+    int delta = CalcLogic.hiku(a, b); //FQCNを指定しても良い
     System.out.println("足すと" + total + "、引くと" + delta);
   }
 }
