299c528f14ff0a65bd0bf4a2f2817936d1ea6d7c
sk99112 <129582261+sk99112k@users.noreply.github.com>
2023-09-05 14:02:07 +0900

    練習問題1

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+  public static void main(String[] args) throws Exception {
+    Zenhan.doWarusa();
+    Zenhan.doTogame();
+    Kouhan.callDeae();
+    Kouhan.showMondokoro();
+  }
+}
