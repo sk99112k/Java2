6c546d77755b4825082c201565b76ea1f95b617e
sk99112 <129582261+sk99112k@users.noreply.github.com>
2023-09-06 14:22:31 +0900

    8章 神様クラス

--- Main.java
+++ Main.java
@@ -1,10 +1,8 @@
-import comment.Zenhan;
-
 public class Main {
-  public static void main(String[] args) throws Exception {
-    Zenhan.doWarusa();
-    Zenhan.doTogame();
-    comment.Kouhan.callDeae();
-    comment.Kouhan.showMondokoro();
+  public static void main (String[] args) {
+    System.out.println("勇者よ、この仮想世界に産まれよ！");
+    System.out.println("お化けきのこよ、この仮想世界に産まれよ！");
+    System.out.println("勇者よ、戦え！");
+    System.out.println("お化けきのこよ、逃げろ！");
   }
 }
