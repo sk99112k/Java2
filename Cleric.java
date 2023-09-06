fbffdf8cb0de7bd3e92b93aed8e780ef2324ad47
sk99112 <129582261+sk99112k@users.noreply.github.com>
2023-09-06 16:40:34 +0900

    第8章 練習問題4

--- Cleric.java
+++ Cleric.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Cleric {
   String name;
   int hp = 50;
@@ -10,5 +12,14 @@ public class Cleric {
     this.mp -= 5;
     this.hp = this.MAX_HP;
     System.out.println("HPが最大まで回復した。");
+  }
+  public int pray(int second) {
+    System.out.print(this.name + "は、" + second +"秒間祈った。");
+    int recover = new Random().nextInt(3) + second;
+    int recoverActual = Math.min(this.MAX_MP - this.mp, recover);
+
+    this.mp += recoverActual;
+    System.out.println("MPが" + recoverActual + "回復した。");
+    return recoverActual;
   }
 }
