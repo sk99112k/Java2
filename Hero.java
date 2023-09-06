22ef35710d8ed2972686a585a60ea87327619f42
sk99112 <129582261+sk99112k@users.noreply.github.com>
2023-09-06 14:52:00 +0900

    8章 登場人物クラス(HeroのHPが座って回復)

--- Hero.java
+++ Hero.java
@@ -6,4 +6,9 @@ public class Hero {
     this.hp = 100;
     System.out.println(this.name + "は、眠って回復した！");
   }
+  public void sit(int sec) {
+    this.hp += sec;
+    System.out.println(this.name + "は、" + sec + "秒座った。");
+    System.out.println("HPが、" + sec + "ポイント回復した。");
+  }
 }
