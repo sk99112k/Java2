62a03bb1b5204e51274f33beb22f9c02ba1ec263
sk99112 <129582261+sk99112k@users.noreply.github.com>
2023-09-06 15:19:47 +0900

    8章 登場人物クラス(slip, run)

--- Main.java
+++ Main.java
@@ -1,8 +1,12 @@
 public class Main {
   public static void main (String[] args) {
-    System.out.println("勇者よ、この仮想世界に産まれよ！");
-    System.out.println("お化けきのこよ、この仮想世界に産まれよ！");
-    System.out.println("勇者よ、戦え！");
-    System.out.println("お化けきのこよ、逃げろ！");
+    Hero h = new Hero();
+    h.name = "みなと";
+    h.hp = 100;
+    System.out.println("勇者" + h.name + "を生み出しました。");
+    // System.out.println("勇者よ、この仮想世界に産まれよ！");
+    // System.out.println("お化けきのこよ、この仮想世界に産まれよ！");
+    // System.out.println("勇者よ、戦え！");
+    // System.out.println("お化けきのこよ、逃げろ！");
   }
 }
