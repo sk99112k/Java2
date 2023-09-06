799172ff16c9a994aa10caac600e58ad8361099a
sk99112 <129582261+sk99112k@users.noreply.github.com>
2023-09-06 16:16:02 +0900

    第8章 練習問題3

--- Cleric.java
+++ Cleric.java
@@ -4,4 +4,11 @@ public class Cleric {
   final int MAX_HP = 50;
   int mp = 10;
   final int MAX_MP = 10;
+
+  public void selfAid() {
+    System.out.println(this.name + "は、セルフエイドを唱えた！");
+    this.mp -= 5;
+    this.hp = this.MAX_HP;
+    System.out.println("HPが最大まで回復した。");
+  }
 }
