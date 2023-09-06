c521f0004a2497cedaedf70e4b317494cc9797dc
sk99112 <129582261+sk99112k@users.noreply.github.com>
2023-09-06 14:58:04 +0900

    8章 登場人物クラス(slip, run)

--- Hero.java
+++ Hero.java
@@ -11,4 +11,14 @@ public class Hero {
     System.out.println(this.name + "は、" + sec + "秒座った。");
     System.out.println("HPが、" + sec + "ポイント回復した。");
   }
+  public void slip() {
+    this.hp -= 5;
+    System.out.println(this.name + "は、転んだ！");
+    System.out.println("HPが5ポイント減った。");
+  }
+  public void run() {
+    System.out.println(this.name + "は、逃げ出した。");
+    System.out.println("GAMEOVER");
+    System.out.println("最終HPは、" + this.hp + "でした。");
+  }
 }
