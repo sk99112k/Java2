0cb431db8f9e89edb7b3e15ce4faa1c4c33485ce
sk99112 <129582261+sk99112k@users.noreply.github.com>
2023-09-08 12:15:40 +0900

    finalをつけることでオーバーライド禁止になる

--- superhero2/Hero.java
+++ superhero2/Hero.java
@@ -6,7 +6,15 @@ public class Hero {
     this.name = name;
     this.hp = 100;
   }
+  //転ぶ
+  //finalとつけることで、slipメソッドは子クラスでオーバーライド禁止
+  public final void slip() {
+    this.hp -= 2;
+    System.out.println("this.name" + "は、転んだ。");
+    System.out.println("2ポイントのダメージを受けた");
+  }
   //戦う
+  //finalがついていないので、attackメソッドは子クラスでオーバーライド可能
   public void attack(Matango m) {
     System.out.println(this.name + "の攻撃");
     m.hp -= 5;
