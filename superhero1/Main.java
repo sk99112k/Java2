c14e1db046b99736668cf987f3528079731e4d4c
sk99112 <129582261+sk99112k@users.noreply.github.com>
2023-09-08 11:25:48 +0900

    SuperHero

---
+++ superhero1/Main.java
@@ -0,0 +1,13 @@
+package superhero1;
+public class Main {
+  public static void main(String[] args) {
+    SuperHero h = new SuperHero();
+    Matango m = new Matango('A');
+
+    h.attack(m);
+    m.run();
+    h.run();
+    h.fly();
+    h.land();
+  }
+}
