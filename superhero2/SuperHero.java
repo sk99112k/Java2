cbeb7dc52d2672b61c650f023012274bd6c195e0
sk99112 <129582261+sk99112k@users.noreply.github.com>
2023-09-08 12:05:16 +0900

    子クラスのSuperHeroでも再度runを定義する

--- superhero2/SuperHero.java
+++ superhero2/SuperHero.java
@@ -4,12 +4,19 @@ public class SuperHero extends Hero {
     super(name); //親クラス Hero のコンストラクタを呼び出す
     this.flying = false; // 初期値は飛んでいない状態
   }
+  //飛ぶ
   public void fly() {
     this.flying = true;
     System.out.println("飛び上がった");
   }
+  //着地する
   public void land() {
     this.flying = false;
     System.out.println("着地した");
+  }
+  //撤退する
+  //Heroにもrunはあるが、子クラスのSuperHeroでも再度runを定義する
+  public void run() {
+    System.out.println(this.name + "は、撤退した！");
   }
 }
